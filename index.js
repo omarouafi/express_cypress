f21f3207ac88449440003e08c6d25ad49bb41a6b
omarouafi <omarouafi12@gmail.com>
2024-04-16 14:30:30 +0200

    update server localhost

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Server is running on http://localhost:3000");
 });
 
